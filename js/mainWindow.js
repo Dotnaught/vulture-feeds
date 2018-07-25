6bf68cbc9b4185bc4536b57b2f75e60d12e89209
THOMAS CLABURN <thomasclaburn@Mournblade.local>
2018-07-25 08:06:36 -0700

    Initial search filtering

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -53,6 +53,25 @@ function setter(val){
     }
 }
 
+function searchFunction() {
+    let input, filter, tr, td, i;
+    input = document.getElementById("query");
+    console.log(input.value);
+    filter = input.value.toUpperCase();
+    //table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
 //turn progress bar on or off
 function toggleProgressBar(bstate){
     let bar = document.getElementsByClassName("determinate")[0] || document.getElementsByClassName("indeterminate")[0];
